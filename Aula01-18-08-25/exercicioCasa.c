92be9f290f4b605f9c9ddd012757f755a191b428
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-08-25 20:20:57 -0300

    Continuando o exercicio da aula 2

---
+++ Aula01-18-08-25/exercicioCasa.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+char inverso(char n[50], int tam) {
+
+    int cont = 0;
+    char stringInvertida[50];
+
+    if (cont == tam) {
+        return stringInvertida[50];
+    } else {
+        
+        for (int i = 0; i < tam; i++) {
+
+            n[tam - 1] = 
+
+        }
+
+    }
+
+}
+
+
+int main() {
+
+    char nome[50];
+    int tamanho;
+
+    printf("Digite o nome: ");
+    scanf("%s", &nome);
+
+    tamanho = strlen(nome);
+
+    printf("O nome e = %s\n", nome);
+    printf("A qtd de letras e = %d\n", tamanho);
+    printf("O indice e = %d\n", tamanho - 1);
+
+    return 0;
+
+}
