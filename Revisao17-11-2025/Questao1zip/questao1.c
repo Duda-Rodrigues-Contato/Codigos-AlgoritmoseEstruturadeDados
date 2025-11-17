1a68a04f470d60300780a493dcdec07bd5942227
Duda-Rodrigues-Contato <mdudarodrigues.contato@gmail.com>
2025-11-17 19:19:27 -0300

    Finalizei a questão 1.

---
+++ Revisao17-11-2025/Questao1zip/questao1.c
@@ -0,0 +1,33 @@
+// Transforme o Busca Sequencial iterativa em recursiva:
+
+#include <stdio.h>
+
+int budcaSequencialRecursiva(int vetor[], int tamanho, int valor) {
+
+    if (tamanho <= 0) {
+
+        return -1;
+
+    } else if (vetor[tamanho - 1] == valor) {
+
+        return tamanho - 1;
+
+    } else {
+
+        return budcaSequencialRecursiva(vetor,tamanho - 1, valor);
+    }
+
+}
+
+
+int main() {
+
+    int vetor[] ={4, 8, 3, 55, 25};
+    int valor = 3;
+    int pos = budcaSequencialRecursiva(vetor, 5, valor);
+
+    printf("O valor %d esta na posicao: %d\n", valor, pos);
+
+    return 0;
+
+}
